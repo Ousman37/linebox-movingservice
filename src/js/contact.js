70826f75fddcf0f08b612315f99efd503d4115b5
Ethmane Didi <ousman.didibela@gmail.com>
2023-06-13 15:02:24 +0200

    Add form validation for name, email, and phone

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,40 +1,41 @@
-$(document).ready(function () {
-  //material contact form
-  $('.contact-form')
-    .find('.form-control')
-    .each(function () {
-      var targetItem = $(this).parent();
-      if ($(this).val()) {
-        $(targetItem).find('label').css({
-          top: '10px',
-          fontSize: '14px',
-        });
-      }
-    });
-  $('.contact-form')
-    .find('.form-control')
-    .focus(function () {
-      $(this).parent('.input-block').addClass('focus');
-      $(this).parent().find('label').animate(
-        {
-          top: '10px',
-          fontSize: '14px',
-        },
-        300
-      );
-    });
-  $('.contact-form')
-    .find('.form-control')
-    .blur(function () {
-      if ($(this).val().length == 0) {
-        $(this).parent('.input-block').removeClass('focus');
-        $(this).parent().find('label').animate(
-          {
-            top: '25px',
-            fontSize: '18px',
-          },
-          300
-        );
-      }
-    });
-});
+function validateForm() {
+  var nameInput = document.getElementById('name');
+  var emailInput = document.getElementById('email');
+  var phoneInput = document.getElementById('phone');
+  var nameError = document.getElementById('nameError');
+  var emailError = document.getElementById('emailError');
+  var phoneError = document.getElementById('phoneError');
+
+  nameError.textContent = '';
+  emailError.textContent = '';
+  phoneError.textContent = '';
+
+  if (nameInput.value.length > 10) {
+    nameError.textContent = 'Name must not exceed 10 characters.';
+    return false;
+  }
+
+  if (!isValidEmail(emailInput.value)) {
+    emailError.textContent = 'Invalid email address.';
+    return false;
+  }
+
+  if (!isValidPhoneNumber(phoneInput.value)) {
+    phoneError.textContent = 'Invalid phone number.';
+    return false;
+  }
+
+  return true;
+}
+
+function isValidEmail(email) {
+  // Simple email validation using a regular expression
+  var emailPattern = /^\S+@\S+\.\S+$/;
+  return emailPattern.test(email);
+}
+
+function isValidPhoneNumber(phone) {
+  // Simple phone number validation using a regular expression
+  var phonePattern = /^\d{10}$/;
+  return phonePattern.test(phone);
+}
